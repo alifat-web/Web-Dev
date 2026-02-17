1169d5b1f19bfbebd785ee96252e186e331c42c2
Alifat <a_sabitov@kbtu.kz>
2026-02-17 16:59:21 +0500

    Update script.js

--- lab3/script.js
+++ lab3/script.js
@@ -1,6 +1,28 @@
 const form = document.getElementById('todo-form');
 const input = document.getElementById('todo-input');
 const todoList = document.getElementById('todo-list');
+const showAllBtn = document.getElementById('show-all');
+const showCompletedBtn = document.getElementById('show-completed');
+
+showAllBtn.addEventListener('click', function () {
+    const items = document.querySelectorAll('.todo-item');
+    items.forEach(function (item) {
+        item.style.display = 'flex';
+    });
+});
+
+showCompletedBtn.addEventListener('click', function () {
+    const items = document.querySelectorAll('.todo-item');
+    items.forEach(function (item) {
+        const checkbox = item.querySelector('input[type="checkbox"]');
+
+        if (checkbox.checked) {
+            item.style.display = 'flex';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+});
 
 form.addEventListener('submit', function (event) {
     event.preventDefault();
